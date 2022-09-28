95e6f38e90fcefb535a679c09e2e6e34e60a2bdc
YotoCoder <yotoelectronics@gmail.com>
2022-09-27 21:10:52 -0500

    outputs

--- app/views.py
+++ app/views.py
@@ -10,25 +10,27 @@ def home(request):
 
 def start(request):
 
-    os.popen('cd .. && cd backend && sudo docker-compose up -d').read()
+    o = os.popen('cd .. && cd backend && sudo docker-compose up -d').read()
 
     return HttpResponseRedirect('/')
 
 
 def stop(request):
 
-    os.popen('cd .. && cd backend && sudo docker-compose stop').read()
+    o = os.popen('cd .. && cd backend && sudo docker-compose stop').read()
 
     return HttpResponseRedirect('/')
 
 def restart(request):
     
-    os.popen('cd .. && cd backend && sudo docker-compose restart').read()
+    o = os.popen('cd .. && cd backend && sudo docker-compose restart').read()
+
+    print(o)
 
     return HttpResponseRedirect('/')
 
 def pull(request):
 
-    os.popen('cd .. && cd backend && sudo docker-compose stop && sudo git pull && sudo docker-compose up -d').read()
+    o = os.popen('cd .. && cd backend && sudo docker-compose stop && sudo git pull && sudo docker-compose up -d').read()
 
     return HttpResponseRedirect('/')
